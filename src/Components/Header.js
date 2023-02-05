9357b36cceeb943a3b807c404ee94f359f0b64a1
Magistry <Magistrigusti@yandex.ru>
2023-02-06 01:47:16 +0200

    0.03

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,53 +1,60 @@
 import React, { Component } from 'react';
-import { Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Navbar, Container, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import Home from './../Pages/Home.js';
+import About from './../Pages/About.js';
+import Contacts from './../Pages/Contacts';
+import Blog from './../Pages/Blog';
 import logo from './logo.svg';
 
 export default class Header extends Component {
   render() {
-    return (
-      <>
+    return(
+      <div>
         <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="/">
-              <img
-                  src={logo}
-                  height="30"
-                  width="30"
-                  className="d-inline-block align-top"
-                  alt="Logo"
-              /> React site
-            </Navbar.Brand>
+        <Container>
+          <Navbar.Brand href="/">
+            <img 
+              src={logo}
+              height="30"
+              width="30"
+              className="d-inline-block alogn-top"
+              alt="logo"
+            /> Magistrigusti site
+          </Navbar.Brand>
 
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/"> Home </Nav.Link>
-                <Nav.Link href="/about"> About Us</Nav.Link>
-                <Nav.Link href="/contacts"> Contacts </Nav.Link>
-                <Nav.Link href="/blog"> Blog </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/"> Home </Nav.Link>
+              <Nav.Link href="/about"> About Us </Nav.Link>
+              <Nav.Link href="/contacts"> Contacts </Nav.Link>
+              <Nav.Link href="/blog"> Blog </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
 
-            <Form inline>
-                <FormControl 
-                    type="text"
-                    plaseholder="Search"
-                    className="mr-sm-2"
-                />
-            </Form>
+          <Form>
+            <FormControl 
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+            />
+          </Form>
 
-            <Button variant="outline-info">Search</Button>
-          </Container>
+          <Button variant="outline-info">Search</Button>
+        </Container>
         </Navbar>
 
         <Router>
-          <Switch>
-            <Route />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
         </Router>
-      </>
+      </div>
     )
   }
 }
--- src/Components/try.js
+++ src/Components/try.js
@@ -2,7 +2,7 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
           <Container>
             <Navbar.Brand href="/">
               <img
@@ -11,32 +11,41 @@ export default class Header extends Component {
                   width="30"
                   className="d-inline-block align-top"
                   alt="Logo"
-              />
+              /> React site
             </Navbar.Brand>
 
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            
+
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
                 <Nav.Link href="/"> Home </Nav.Link>
-                <Nav.Link href="/about"> About Us </Nav.Link>
+                <Nav.Link href="/about"> About Us</Nav.Link>
                 <Nav.Link href="/contacts"> Contacts </Nav.Link>
                 <Nav.Link href="/blog"> Blog </Nav.Link>
               </Nav>
+            </Navbar.Collapse>
 
-              <Form inline>
+            <Form>
                 <FormControl 
                     type="text"
-                    placeholder="Search" 
+                    plaseholder="Search"
                     className="mr-sm-2"
                 />
+            </Form>
 
-                <Button variant="outline-info">Search</Button>
-              </Form>
-            </Navbar.Collapse>
+            <Button variant="outline-info">Search</Button>
           </Container>
         </Navbar>
+
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </Router>
       </div>
-    );
+    )
   }
 }
