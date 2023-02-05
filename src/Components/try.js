7be7a4b28ff264c7fe17adfd63d8ab3f8657a1d1
Magistry <Magistrigusti@yandex.ru>
2023-02-05 14:38:14 +0200

    0.02

--- src/Components/try.js
+++ src/Components/try.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
-import { Router, Switch, Route} from 'react-router-dom';
-import {Navbar, Container, Nav, Form, FormControl, Button} from 'react-bootstrap';
-import logo from './logo.svg';
-
 export default class Header extends Component {
   render() {
     return (
-      <>
-        <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+      <div>
+        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
           <Container>
             <Navbar.Brand href="/">
               <img
@@ -16,38 +11,32 @@ export default class Header extends Component {
                   width="30"
                   className="d-inline-block align-top"
                   alt="Logo"
-              /> React site
+              />
             </Navbar.Brand>
 
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
+            
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
                 <Nav.Link href="/"> Home </Nav.Link>
-                <Nav.Link href="/about"> About Us</Nav.Link>
+                <Nav.Link href="/about"> About Us </Nav.Link>
                 <Nav.Link href="/contacts"> Contacts </Nav.Link>
                 <Nav.Link href="/blog"> Blog </Nav.Link>
               </Nav>
-            </Navbar.Collapse>
 
-            <Form inline>
+              <Form inline>
                 <FormControl 
                     type="text"
-                    plaseholder="Search"
+                    placeholder="Search" 
                     className="mr-sm-2"
                 />
-            </Form>
 
-            <Button variant="outline-info">Search</Button>
+                <Button variant="outline-info">Search</Button>
+              </Form>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
-
-        <Router>
-          <Switch>
-            <Route />
-          </Switch>
-        </Router>
-      </>
-    )
+      </div>
+    );
   }
 }
